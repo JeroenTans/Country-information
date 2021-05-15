381e45b3656a82e0301eba3a3866d38008e1b148
JeroenTans <jeroentans1@gmail.com>
2021-05-15 14:01:29 +0200

    tot vraag 12

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 
 
-
 const container = document.getElementById('pictureFlag');
 const containerTwo = document.getElementById('countryInfo')
-
+const containerThree = document.getElementById('errorMessage')
+const inputField = document.getElementById('searchCountry');
 const countryFlagg = document.createElement('img');
 countryFlagg.style.width = "400px";
 countryFlagg.style.height = "210px";
 
-/*
-const inputField = document.getElementById('searchCountry');
-inputField.addEventListener('keydown', allCode)
-*/
+function whatCurrencies (currencies) {
+    let arrayCurrency = [];
+    for (let i = 0; i < currencies.length; i++) {
+        arrayCurrency[i] = currencies[i].name
+    }
+    return arrayCurrency.join(" and ")
+}
 
-const searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', async function () {
+function whatLanguage (languages) {
+    let arrayLanguages = [];
+    for (let i = 0; i < languages.length; i++) {
+        arrayLanguages[i] = languages[i].name;
+    }
+    return arrayLanguages.join(" and ");
+}
 
-    const result = await axios.get('https://restcountries.eu/rest/v2/name/belgie');
+function countryInfo (flagOfCountry, nameOfCountry, region, amountOfPeople, capital, currencies, languages) {
+
+    container.appendChild(countryFlagg);
+    countryFlagg.setAttribute('src', flagOfCountry);
+
+    containerTwo.textContent = nameOfCountry + " is situated in " + region + ". It has a population of " + amountOfPeople + " people. The capital is " + capital + " and you can pay with " + whatCurrencies(currencies) + "'s. They speak " + whatLanguage(languages);
+
+    return inputField.value = "";
+}
+
+async function allCode () {
+
+    const result = await axios.get(`https://restcountries.eu/rest/v2/name/${inputField.value}`);
 
     try {
         const data = result.data[0];
@@ -25,60 +45,20 @@ searchButton.addEventListener('click', async function () {
         const amountOfPeople = data.population
         const capital = data.capital
         const currencies = data.currencies;
-        let arrayCurrency = [];
-        let arrayLanguages = [];
         const languages = data.languages
         const flagOfCountry = data.flag
 
-        function whatCurrencies () {
-            for (let i = 0; i < data.currencies.length; i++) {
-                arrayCurrency[i] = currencies[i].name
-            }
-            return arrayCurrency.join(" and ")
-        }
-
-        function whatLanguage () {
-            for (let i = 0; i < data.languages.length; i++) {
-                arrayLanguages[i] = languages[i].name;
-            }
-            return arrayLanguages.join(" and ");
-        }
-
-        function countryInfo () {
-
-            //container.appendChild(nameOfCountry)
-            container.appendChild(countryFlagg);
-            countryFlagg.setAttribute('src', flagOfCountry);
-
-            containerTwo.textContent = nameOfCountry + " is situated in " + region + ". It has a population of " + amountOfPeople + " people. The capital is " + capital + " and you can pay with " + whatCurrencies(currencies) + "'s. They speak " + whatLanguage();
-        }
-
-        countryInfo()
+        countryInfo(flagOfCountry, nameOfCountry, region, amountOfPeople, capital, currencies, languages)
     } catch (e) {
         console.log('Kan dit land momenteel niet laden');
     }
+}
 
-})
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', allCode)
+
+inputField.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') allCode();})
 
 
 
-
-
-
-
-
-//let currency = [];
-/* console.log("lengte te van currency array: ", array.length);*/
-//for (let i = 0; i < response.data[0].currencies.length; i++) {
-//    if (response.data[0].currencies[i].name === false) {
-//        i++
-//    } else {
-//       currency[i] = response.data[0].currencies[i].name
-//    }
-//    if (currency[i].length > 1) {
-//        console.log(currency[i] + " and ");
-//    } else {
-//        console.log(currency[i]);
-//        /*console.log("dit moeten alle losse currency's zijn:", currency[i]);*/
-//    }
-// }
